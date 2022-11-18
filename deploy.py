e6f1da6f63c75fe47644e3d5ef815d1ae5612526
CipherSharma <79153600+CipherSharma@users.noreply.github.com>
2022-11-18 22:36:30 +0530

    first push
    first push

--- deploy.py
+++ deploy.py
@@ -1,36 +1,16 @@
-'''
-CODER ZERO
-connect with me at: https://www.youtube.com/channel/UCKipQAvBc7CWZaPib4y8Ajg
-How to train custom yolov5: https://youtu.be/12UoOlsRwh8
-DATASET: 1) https://www.kaggle.com/datasets/deepakat002/indian-vehicle-number-plate-yolo-annotation
-         2) https://www.kaggle.com/datasets/elysian01/car-number-plate-detection
-'''
-### importing required libraries
 import torch
 import cv2
-import time
-# import pytesseract
-import re
 import numpy as np
 import easyocr
 
-
-##### DEFINING GLOBAL VARIABLE
-EASY_OCR = easyocr.Reader(['en']) ### initiating easyocr
+EASY_OCR = easyocr.Reader(['en']) 
 OCR_TH = 0.2
-
-
-
 
 ### -------------------------------------- function to run detection ---------------------------------------------------------
 def detectx (frame, model):
     frame = [frame]
     print(f"[INFO] Detecting. . . ")
     results = model(frame)
-    # results.show()
-    # print( results.xyxyn[0])
-    # print(results.xyxyn[0][:, -1])
-    # print(results.xyxyn[0][:, :-1])
 
     labels, cordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
 
@@ -38,20 +18,12 @@ def detectx (frame, model):
 
 ### ------------------------------------ to plot the BBox and results --------------------------------------------------------
 def plot_boxes(results, frame,classes):
-
-    """
-    --> This function takes results, frame and classes
-    --> results: contains labels and coordinates predicted by model on the given frame
-    --> classes: contains the strting labels
-
-    """
     labels, cord = results
     n = len(labels)
     x_shape, y_shape = frame.shape[1], frame.shape[0]
 
     print(f"[INFO] Total {n} detections. . . ")
     print(f"[INFO] Looping through all detections. . . ")
-
 
     ### looping through the detections
     for i in range(n):
@@ -60,42 +32,26 @@ def plot_boxes(results, frame,classes):
             print(f"[INFO] Extracting BBox coordinates. . . ")
             x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape) ## BBOx coordniates
             text_d = classes[int(labels[i])]
-            # cv2.imwrite("./output/dp.jpg",frame[int(y1):int(y2), int(x1):int(x2)])
 
             coords = [x1,y1,x2,y2]
 
             plate_num = recognize_plate_easyocr(img = frame, coords= coords, reader= EASY_OCR, region_threshold= OCR_TH)
 
-
-            # if text_d == 'mask':
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) ## BBox
             cv2.rectangle(frame, (x1, y1-20), (x2, y1), (0, 255,0), -1) ## for text label background
             cv2.putText(frame, f"{plate_num}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255), 2)
 
-            # cv2.imwrite("./output/np.jpg",frame[int(y1)-25:int(y2)+25, int(x1)-25:int(x2)+25])
-
-
-
-
     return frame
-
 
 
 #### ---------------------------- function to recognize license plate --------------------------------------
 
-
 # function to recognize license plate numbers using Tesseract OCR
 def recognize_plate_easyocr(img, coords,reader,region_threshold):
-    # separate coordinates from box
     xmin, ymin, xmax, ymax = coords
-    # get the subimage that makes up the bounded region and take an additional 5 pixels on each side
-    # nplate = img[int(ymin)-5:int(ymax)+5, int(xmin)-5:int(xmax)+5]
     nplate = img[int(ymin):int(ymax), int(xmin):int(xmax)] ### cropping the number plate from the whole image
 
-
     ocr_result = reader.readtext(nplate)
-
-
 
     text = filter_text(region=nplate, ocr_result=ocr_result, region_threshold= region_threshold)
 
@@ -120,23 +76,14 @@ def filter_text(region, ocr_result, region_threshold):
     return plate
 
 
-
-
-
 ### ---------------------------------------------- Main function -----------------------------------------------------
 
 def main(img_path=None, vid_path=None,vid_out = None):
 
     print(f"[INFO] Loading model... ")
-    ## loading the custom trained model
-    # model =  torch.hub.load('ultralytics/yolov5', 'custom', path='last.pt',force_reload=True) ## if you want to download the git repo and then run the detection
-    model =  torch.hub.load('./yolov5-master', 'custom', source ='local', path='best.pt',force_reload=True) ### The repo is stored locally
-
-    classes = model.names ### class names in string format
-
-
-
-
+    model =  torch.hub.load('./yolov5', 'custom', source ='local', path='best.pt',force_reload=True) 
+    classes = model.names
+    
     ### --------------- for detection on image --------------------
     if img_path != None:
         print(f"[INFO] Working with image: {img_path}")
@@ -155,14 +102,12 @@ def main(img_path=None, vid_path=None,vid_out = None):
         cv2.namedWindow("img_only", cv2.WINDOW_NORMAL) ## creating a free windown to show the result
 
         while True:
-            # frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
-
             cv2.imshow("img_only", frame)
 
             if cv2.waitKey(5) & 0xFF == ord('q'):
                 print(f"[INFO] Exiting. . . ")
 
-                cv2.imwrite(f"{img_out_name}",frame) ## if you want to save he output result.
+                cv2.imwrite(f"{img_out_name}",frame)
 
                 break
 
@@ -176,19 +121,16 @@ def main(img_path=None, vid_path=None,vid_out = None):
 
         if vid_out: ### creating the video writer if video output path is given
 
-            # by default VideoCapture returns float instead of int
             width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fps = int(cap.get(cv2.CAP_PROP_FPS))
-            codec = cv2.VideoWriter_fourcc(*'mp4v') ##(*'XVID')
+            codec = cv2.VideoWriter_fourcc(*'mp4v')
             out = cv2.VideoWriter(vid_out, codec, fps, (width, height))
 
-        # assert cap.isOpened()
         frame_no = 1
 
         cv2.namedWindow("vid_out", cv2.WINDOW_NORMAL)
         while True:
-            # start_time = time.time()
             ret, frame = cap.read()
             if ret  and frame_no %1 == 0:
                 print(f"[INFO] Working with frame {frame_no} ")
@@ -220,10 +162,7 @@ def main(img_path=None, vid_path=None,vid_out = None):
 
 ### -------------------  calling the main function-------------------------------
 
-
 # main(vid_path="./test_images/vid_1.mp4",vid_out="vid_1.mp4") ### for custom video
-# main(vid_path=0,vid_out="webcam_facemask_result.mp4") #### for webcam
-
-main(img_path="./test_images/Cars74.jpg") ## for image
+main(img_path="./test_images/Cars116.jpg") 
             
 
